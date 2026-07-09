7f110bdb3bf07ef0966a1ae93bf71baebea245fa
Irawati16 <irawatii553@gmail.com>
2026-07-09 10:41:18 +0700

    notifikasi struktur
    udah disamain seperti mitra

--- public/js/admin/struktur.js
+++ public/js/admin/struktur.js
@@ -50,4 +50,24 @@ searchInput.addEventListener('input', function () {
     filterForm.submit();
 });
 
+function showNotification(message, type = "success") {
 
+    const notification = document.getElementById("notification");
+
+    let icon = "";
+
+    if(type === "success"){
+        icon = '<i class="fa-solid fa-circle-check"></i>';
+    }else if(type === "error"){
+        icon = '<i class="fa-solid fa-circle-xmark"></i>';
+    }else{
+        icon = '<i class="fa-solid fa-circle-info"></i>';
+    }
+
+    notification.innerHTML = `${icon}<span>${message}</span>`;
+    notification.className = `notification ${type} show`;
+
+    setTimeout(() => {
+        notification.classList.remove("show");
+    }, 3000);
+}
