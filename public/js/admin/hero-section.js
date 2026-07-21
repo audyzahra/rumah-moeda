2041e1748a70ed8ceef3e2a0c10b0a53b4d9fe27
audyzahra <audyzahra21@gmail.com>
2026-07-21 14:43:22 +0700

    Hero Preview

--- public/js/admin/hero-section.js
+++ public/js/admin/hero-section.js
@@ -1,3 +1,27 @@
 document.getElementById("heroPreview").addEventListener("click", function () {
     document.getElementById("heroImage").click();
 });
+function previewHero(event) {
+
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+
+        document.getElementById("heroPreview").innerHTML = `
+            <div class="hero-preview-content">
+                <img
+                    src="${e.target.result}"
+                    alt="Preview Hero"
+                    class="hero-preview-image">
+            </div>
+        `;
+
+    };
+
+    reader.readAsDataURL(file);
+
+}
