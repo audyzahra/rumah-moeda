88820c1549426dd382c0636aa9f54e0c8a8b7605
audyzahra <audyzahra21@gmail.com>
2026-08-04 09:41:14 +0700

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,12 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 
 use App\Models\Setting;
 use App\Models\ContactMessage;
-
-use Illuminate\Support\Facades\Schema;
-
 use App\Models\WebsiteVisitor;
 
 use Carbon\Carbon;
@@ -33,24 +31,51 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrapFive();
 
         View::share('setting', Setting::first());
-        View::share(
-            'totalVisitors',
-            WebsiteVisitor::count()
-        );
-        View::share(
-            'todayVisitors',
-            WebsiteVisitor::whereDate(
-                'visit_date',
-                Carbon::today()
-            )->count()
-        );
+
+        // ===========================
+        // Total Pengunjung
+        // ===========================
+
+        if (Schema::hasTable('website_visitors')) {
+
+            View::share(
+                'totalVisitors',
+                WebsiteVisitor::count()
+            );
+
+            View::share(
+                'todayVisitors',
+                WebsiteVisitor::whereDate(
+                    'visit_date',
+                    Carbon::today()
+                )->count()
+            );
+
+        } else {
+
+            View::share('totalVisitors', 0);
+            View::share('todayVisitors', 0);
+
+        }
+
+        // ===========================
+        // Notifikasi Sidebar
+        // ===========================
 
         $jumlahNotifSidebar = 0;
 
         if (Schema::hasTable('contact_messages')) {
-            $jumlahNotifSidebar = ContactMessage::where('is_read', 0)->count();
+
+            $jumlahNotifSidebar = ContactMessage::where(
+                'is_read',
+                0
+            )->count();
+
         }
 
-        View::share('jumlahNotifSidebar', $jumlahNotifSidebar);
+        View::share(
+            'jumlahNotifSidebar',
+            $jumlahNotifSidebar
+        );
     }
 }
