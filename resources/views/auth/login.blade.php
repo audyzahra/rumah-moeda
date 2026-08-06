75842d14b757e627b7defe1a40d158a59152c2d3
Evimeliyani <evimeliyani016@gmail.com>
2026-08-06 09:17:24 +0700

    perbaruan ingat saya ditambahin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -85,6 +85,18 @@
 
                     </div>
 
+                    <div style="margin-bottom:20px;">
+
+                        <label style="display:flex;align-items:center;gap:8px;cursor:pointer;">
+
+                            <input type="checkbox" name="remember">
+
+                            Ingat Saya
+
+                        </label>
+
+                    </div>
+
                     <div class="forgot">
 
                         @if (Route::has('password.request'))
